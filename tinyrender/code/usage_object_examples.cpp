fa00ec72a20356445d6b49696054ab999e922840
harveynw <harveywilliams@live.com>
2023-12-02 17:09:30 +0000

    Prefix

--- tinyrender/code/usage_object_examples.cpp
+++ tinyrender/code/usage_object_examples.cpp
@@ -1,14 +1,14 @@
 // OBJ file
-auto obj = std::make_shared<engine::Mesh>("path/to/file.obj");
+auto obj = std::make_shared<tinyrender::Mesh>("path/to/file.obj");
 
 // Basic geometry
-auto cube = std::make_shared<engine::Cube>();
-auto pyramid = std::make_shared<engine::Pyramid>(glm::vec3 center, float height, float width);
-auto triangle = std::make_shared<engine::Triangle>(glm::vec3 a, glm::vec3 b, glm::vec3 c);
-auto quad = std::make_shared<engine::Quad>(glm::vec3 a, glm::vec3 b, glm::vec3 c, glm::vec3 d);
+auto cube = std::make_shared<tinyrender::Cube>();
+auto pyramid = std::make_shared<tinyrender::Pyramid>(glm::vec3 center, float height, float width);
+auto triangle = std::make_shared<tinyrender::Triangle>(glm::vec3 a, glm::vec3 b, glm::vec3 c);
+auto quad = std::make_shared<tinyrender::Quad>(glm::vec3 a, glm::vec3 b, glm::vec3 c, glm::vec3 d);
 
 // Wave Simulation
-auto waveSim = std::make_shared<engine::WaveSim>(float height, float width);
+auto waveSim = std::make_shared<tinyrender::WaveSim>(float height, float width);
 
 // Voxel rendering
-auto voxels = std::make_shared<engine::Voxels>();
+auto voxels = std::make_shared<tinyrender::Voxels>();
